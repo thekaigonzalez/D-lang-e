b3deb13a636b6f01b91f4828656fe62981ec2d12
Kai D. Gonzalez <gkai70264@gmail.com>
2021-08-29 13:19:55 -0400

    ???

---
+++ main.d
@@ -0,0 +1,12 @@
+
+#!/usr/bin/dmd
+import std.process;
+import std.stdio : writeln, readln;
+import std.string;
+import std.system;
+void main()
+{
+ char[] command;
+ readln(command);
+ writeln(os);
+}
